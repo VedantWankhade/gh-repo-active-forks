209cf6c007e5433ed02cc28479e1cd9a1274da30
VedantWankhade <vedantwankhade17@gmail.com>
2021-10-28 14:48:20 +0530

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # gh-repo-active-forks
-A browser extension that displays active forks of a github repository on its main page.
+A browser extension that displays active forks of a github repository.
--- src/popup.js
+++ src/popup.js
@@ -5,11 +5,12 @@ chrome.runtime.sendMessage({ reqMessage: "get-forks" }, (res) => {
     console.log("Recieved message from background script: ", res);
 
     let forksListHtml = "";
-
     res.forEach((fork => {
-        console.log(fork.html_url, fork.full_name)
+
+        // console.log(fork.html_url, fork.full_name)
         forksListHtml += `<li><a href="${fork.html_url}" target="_blank">${fork.full_name}</a> :  ${fork.stargazers_count}</li>`;
     }));
+    
     // console.log(forksListHtml)
     forksList.innerHTML = forksListHtml;
 });
